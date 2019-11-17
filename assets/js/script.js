badd52d0539c31ad2f706bd05d3b18dc9d72c38b
Elisa Munoz <elisamunozes@gmail.com>
2019-11-17 22:53:44 +0000

    Add home website and assets folder

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $("#search").click(searchByName);
+
+    $("#surpriseMe").click(searchRandomly);
+});
+
+
+function searchByName(){
+    console.log("TODO: add Search functionality")
+}
+
+function searchRandomly(){
+    console.log("TODO: add Surprise me functionality")
+}
