e09ed45fbaf140310c2a38ca3587297f9cfeb066
Elisa Munoz <elisamunozes@gmail.com>
2019-12-01 16:57:12 +0000

    suggestions carrousel

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
-# birdsongs-project
-Birdsongs of the World
+#Birdsongs of the World.
+
+
+
+Birdsongs of the World is a website dedicated to sharing bird sounds from all over the world. Whether you are a research scientist, a birder, or simply curious about a sound that you heard out your kitchen window, we invite you to listen, download, and explore the bird sound recordings in the collection.
+
+This website has been possible thanks to the [Xeno-Canto Api](https://www.xeno-canto.org/article/153).
+
+
+## UX
+
+My goal is to create an interactive frontend development project
+
+
+
+## Technologies
+
+HTML
+CSS
+Bootstrap (4.3.1)
+Font Awesome (5.11.2)
+Google Fonts
+Javascript
+JQuery
+
+
+## Features
+
+
+
+## Testing
+
+
+
+## Deployment
+
+This website is hosted and deployed using GitHub pages. The link to my website is: [Birdsongs of the World](https://elisamunoz.github.io/birdsongs-project/)
+
+
+
+## Credits
+
+This website has been possible to the [Xeno-Canto API](https://www.xeno-canto.org/article/153). All images were taken from [Pixabay](https://pixabay.com/) and [Google Images](https://images.google.com/)
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,130 +1,128 @@
 // Call  functions
-$(document).ready(function(){
-    $("#search-bird").click(searchBird);
-    $("#surpriseMe").click(searchRandomly);
-    initMap();
+$(document).ready(function() {
+  $("#search-bird").click(searchBird);
+  $("#surpriseMe").click(searchRandomly);
+  initMap();
 });
 
 /* --------------------- */
 /* --- Get Info -------- */
 /* --------------------- */
 function searchBird() {
-    var country = $("#search-by-country").val();
-    var genus = $("#input-by-name").val();
-    var text = "";
-    var textCountry = `Birds from ${country}`
-    var textGenus = `Birds with genus ${genus}`
-    var textComplete = `Birds from ${country} with genus ${genus}`
+  var country = $("#search-by-country").val();
+  var genus = $("#input-by-name").val();
+  var text = "";
+  var textCountry = `Birds from ${country}`;
+  var textGenus = `Birds with genus ${genus}`;
+  var textComplete = `Birds from ${country} with genus ${genus}`;
+  var queryCountry = "";
+  var queryGenus = "";
 
-    if (country) { 
-        text = textCountry;
-    }
-    if(genus) {
-        text = textGenus;
-    }
-    if(country && genus) {
-        text = textComplete;
-    }
+  if (country && genus) {
+    text = textComplete;
+    queryCountry = `cnt:${country}`;
+    queryGenus = `+gen:${genus}`;
+  } else if (country) {
+    queryCountry = `cnt:${country}`;
+    text = textCountry;
 
-    var queryCountry = '';
-    var queryGenus = '';
+    // text will be overwritten, genus has priority over country
+  } else if (genus) {
+    queryGenus = `gen:${genus}`;
+    text = textGenus;
+  } else {
+    alert("Error, to do a search, you need to complete any of the fields.");
+  }
 
-    if(country) {
-        queryCountry = `cnt:${country}`;
-    }
-    if(genus) {
-        queryGenus = `+gen:${genus}`;
-    }
-    var url = `https://cors-anywhere.herokuapp.com/https://www.xeno-canto.org/api/2/recordings?query=${queryCountry}${queryGenus}`;
+  var url = `https://cors-anywhere.herokuapp.com/https://www.xeno-canto.org/api/2/recordings?query=${queryCountry}${queryGenus}`;
 
-    getApiData(url, renderTable)
-    renderTitle(text);
+  getApiData(url, renderTable);
+  renderTitle(text);
 }
 
-function searchRandomly(){
-    console.log("TODO: add Surprise me functionality")
+function searchRandomly() {
+  console.log("TODO: add Surprise me functionality");
 }
 
 /* --------------------- */
 /* --- Render Table ---- */
 /* --------------------- */
 function renderTitle(text) {
-    const birdTitle = document.getElementById("title-search-result");
+  const birdTitle = document.getElementById("title-search-result");
 
-    birdTitle.innerHTML = text;
+  birdTitle.innerHTML = text;
 }
 
-
 function renderTable(birdInfo = {}) {
-    const birdsList = birdInfo.recordings;
+  const birdsList = birdInfo.recordings;
 
-    var tableBody = document.getElementById("tableBody");
-    var totalRows = [];
+  var tableBody = document.getElementById("tableBody");
+  var totalRows = [];
 
-    birdsList.forEach(function(birdData){
-        totalRows.push(renderTableRow(birdData));
-    });
+  birdsList.forEach(function(birdData) {
+    totalRows.push(renderTableRow(birdData));
+  });
 
-    tableBody.innerHTML = totalRows.join("");
+  tableBody.innerHTML = totalRows.join("");
 
-    $(".search-result").fadeIn(1000);
-    smoothieScrollTo("search-result");
+  //adds smoothie scroll to search result section
+  $(".search-result").fadeIn(1000);
+  smoothieScrollTo("search-result");
 }
 
 function renderTableRow(birdInfo = {}) {
-
-    return `
+  return `
     <tr>
-    <td onClick="renderFileInfo(${birdInfo.id});" class="bold-text bird-id">${birdInfo.id}</td>
-    <td class="d-none d-lg-block"><i>${birdInfo.gen} ${birdInfo.sp}</i></td>
+    <td onClick="renderFileInfo(${birdInfo.id});" class="bold-text bird-id">More Information</td>
+    <td><i>${birdInfo.gen} ${birdInfo.sp}</i></td>
     <td>${birdInfo.en}</td>
     <td>${birdInfo.length} min</td>
-    <td class="d-none d-lg-block">${birdInfo.cnt}</td>
-    <td class="type">${birdInfo.type}</td>
+    <td>${birdInfo.cnt}</td>
+    <td>${birdInfo.type}</td>
   </tr>
-    `
+    `;
 }
-
-
 
 function renderFileInfo(birdId) {
-    var url = `https://cors-anywhere.herokuapp.com/https://www.xeno-canto.org/api/2/recordings?query=nr:${birdId}`;
-    var urlIFrame = `https://www.xeno-canto.org/${birdId}/embed?simple=1`
-    var fileBody = document.getElementById("fileBody");
-    var fileTitle = document.getElementById("fileTitle");
-    var fileSubTitle = document.getElementById("fileSubTitle");
-    var reproductor = $("#reproductor"); 
+  var url = `https://cors-anywhere.herokuapp.com/https://www.xeno-canto.org/api/2/recordings?query=nr:${birdId}`;
+  var urlIFrame = `https://www.xeno-canto.org/${birdId}/embed?simple=1`;
+  var fileBody = document.getElementById("fileBody");
+  var fileTitle = document.getElementById("fileTitle");
+  var fileSubTitle = document.getElementById("fileSubTitle");
+  var reproductor = $("#reproductor");
 
-    reproductor.attr('src',urlIFrame); 
+  reproductor.attr("src", urlIFrame);
 
-    getApiData(url, function (response) {
-        const birdData = response.recordings[0];
-        fileBody.innerHTML = renderBirdFile(birdData);
-        fileTitle.innerText = birdData.en;
-        fileSubTitle.innerText = birdData.gen +" "+ birdData.sp;
+  getApiData(url, birdFileSection);
 
-        if (!birdData.lat || !birdData.lng){
-            $(".location-map").hide();
-            $(".no-location-image").fadeIn(1000);
-
-        }else{
-            $(".no-location-image").hide();
-            $(".location-map").fadeIn(1000);
-           
-            setMapMarker({
-                lat: parseInt(birdData.lat),
-                lng: parseInt(birdData.lng)
-            });
-        }
-    });
-    $(".bird-file").fadeIn(1000);
-    smoothieScrollTo("bird-file");
+  // add smothie scroll to Bird File section
+  $(".bird-file").fadeIn(1000);
+  smoothieScrollTo("bird-file");
 }
 
+function birdFileSection(response) {
+  const birdData = response.recordings[0];
+  fileBody.innerHTML = renderBirdFile(birdData); // renders
+  fileTitle.innerText = birdData.en; // renders title
+  fileSubTitle.innerText = birdData.gen + " " + birdData.sp; // renders sub title
+
+  // renders map and no location image
+  if (!birdData.lat || !birdData.lng) {
+    $(".location-map").hide();
+    $(".no-location-image").fadeIn(1000);
+  } else {
+    $(".no-location-image").hide();
+    $(".location-map").fadeIn(1000);
+
+    setMapMarker({
+      lat: parseInt(birdData.lat),
+      lng: parseInt(birdData.lng)
+    });
+  }
+}
 
 function renderBirdFile(birdInfo) {
-
-    return `
+  return `
     <tr>
         <td class="bold-text">Register:</td>
         <td>${birdInfo.id}</td>
@@ -179,62 +177,67 @@ function renderBirdFile(birdInfo) {
         <td class="bold-text">Type:</td>
         <td>${birdInfo.type}</td>
     </tr>
-    `
+    `;
 }
-
 
 /* -------------------- */
 /* --- Helper Funcs --- */
 /* -------------------- */
 function getApiData(
-    url, // String
-    cb   // function
+  url, // String
+  cb // function
 ) {
-    var xhr = new XMLHttpRequest();
+  var xhr = new XMLHttpRequest();
 
-    xhr.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            cb(JSON.parse(this.responseText));       
-        }
-    };
+  xhr.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      cb(JSON.parse(this.responseText));
 
-    xhr.open("GET", url);
-    xhr.send();
+      xhr.onerror = function(e) {
+        alert("Error Status: " + e.target.status);
+      };
+    }
+  };
+
+  xhr.open("GET", url);
+  xhr.send();
 }
 
 /* -------------------- */
 /* --- Render Map  --- */
 /* -------------------- */
 function initMap() {
-
-    setMapMarker({lat: -38.6417, lng: -71.7017});
+  setMapMarker({ lat: -38.6417, lng: -71.7017 });
 }
 
 function setMapMarker(position = {}) {
-
-    // The map, centered at position
-    var map = new google.maps.Map(
-        document.getElementById('map'), {zoom: 6, center: position}
-    );
-    // The marker, positioned at Uluru
-    new google.maps.Marker({
-        position: position,
-        map: map
-    });
+  // The map, centered at position
+  var map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 6,
+    center: position
+  });
+  // The marker, positioned at Uluru
+  new google.maps.Marker({
+    position: position,
+    map: map
+  });
 }
 
-function mapNotFound(img){
-    var errorImg = document.getElementById('map');
+function mapNotFound(img) {
+  var errorImg = document.getElementById("map");
 }
-
 
 function smoothieScrollTo(id) {
-    
-    var target = $(`#${id}`);
+  var target = $(`#${id}`);
 
-    if( target.length ) {
-        $('html, body').stop().animate({
-            scrollTop: target.offset().top
-        }, 1000);
-    }
+  if (target.length) {
+    $("html, body")
+      .stop()
+      .animate(
+        {
+          scrollTop: target.offset().top
+        },
+        1000
+      );
+  }
 }
